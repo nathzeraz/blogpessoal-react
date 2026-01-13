66f989bf97d7317f72912cd5ab00a21c10cc1d86
nathalia <oliveiranathalia.mor@gmail.com>
2026-01-13 18:55:22 -0300

    feat: criando o componente home

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/home/Home'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <Home />
+      
+    </>
+  );
+}
+
+export default App
